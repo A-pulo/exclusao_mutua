22661e0e293642657ac9e55a90f56fa1e58598c7
DESKTOP-FV5Q462\Pichau <mineia.mini14@gmail.com>
2021-08-26 13:53:43 -0300

    Exclusao mutua centralizado

--- main.py
+++ main.py
@@ -1,36 +1,188 @@
 import time
 from random import randint
-import asyncio
-import threading
+from threading import Thread
 
-# Classe processo
-
-class Processo:
+#Classe Processo
+class Processo():
     def __init__(self, id):
-        self.__id = id
-        self.__ts = time.perf_counter()
-        self.__tag = f'Processo {id}:'
-        threading.Thread(target=self.run_p).start()
+        self.id = id
+        self.tag = f'Processo {id}:'
+        self.coordenador = None
+        self.isAtivo = True
+        Thread(target=self.run_p).start()
 
     def run_p(self):
-        print(f'{self.__tag} Inicializado!')
+        print(f'{self.tag} Inicializado!')
+        while self.isAtivo:
+            self.consumir_recurso()
+            time.sleep(randint(10, 25))
+
+    def set_coordenador(self, coordenador):
+        self.coordenador = coordenador
+
+    def set_ativo(self, ativo):
+        self.isAtivo = ativo
+
+    def stop(self):
+        del self
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+    def consumir_recurso(self):
+        coordenador = self.coordenador
+        if coordenador is None:
+            Processos().gera_novo_coordenador()
+        elif coordenador is not None and self.id != self.coordenador.id:
+            print('\n')
+            print(f'{self.tag} Solicita acesso do recurso ao coordenador {coordenador.id}!')
+            if coordenador.isRecursoHabilitado == False:
+                self.processa_recurso()
+            else:
+                coordenador.fila.append(self)
+                print(f'******** Fila do coordenador = {self.fila_coordenador(coordenador)}')
+                valida = True
+                while valida:
+                    if coordenador.isRecursoHabilitado == False and coordenador.fila[0].id == self.id:
+                        self.processa_recurso()
+                        valida = False
+
+    def processa_recurso(self):
+        coordenador = self.coordenador
+        if coordenador is not None and coordenador.isRecursoHabilitado == False:
+            print(f'******** Coordenador {coordenador.id} concede acesso ao processo {self.id}')
+            print(f'{self.tag} Iniciado processo do recurso!')
+            coordenador.isRecursoHabilitado = True
+            sleep = randint(5, 15)
+            time.sleep(sleep)
+            print(f'{self.tag} Recurso processado em {sleep}s!')
+            print(
+                f'******** O processo {self.id} informa ao coordenador que o recurso foi liberado')
+            print('\n')
+            coordenador.isRecursoHabilitado = False
+            self.remover_fila(coordenador)
+
+    def remover_fila(self, coordenador):
+        for f in coordenador.fila:
+            if f.id == self.id:
+                coordenador.fila.remove(self)
+
+    def fila_coordenador(self, coordenador):
+        s =[]
+        for f in coordenador.fila:
+            s.append(f.id)
+        return s
+
+class Coordenador:
+    def __init__(self, id):
+        self.id = id
+        self.isRecursoHabilitado = False
+        self.fila = []
+
+    def stop(self):
+        del self
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+class Singleton:
+    _instance = None
+
+    @classmethod
+    def instance(cls):
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
+
+class Processos(Singleton):
+    processos = []
+
+    def gera_processo(self):
         while (True):
-            time.sleep(randint(2,5))
-            print(f'{self.__tag} ativo á {self.get_uptime()} segundos')
+            valida = False
+            while valida == False:
+                ran_id = randint(0, 2048)
+                valida = self.verifica_id_existente(ran_id)
+                processo = Processo(ran_id)
+                processo.set_coordenador(self.get_coordenador())
+                self.processos.append(processo)
+                time.sleep(40)
 
-    def get_uptime(self):
-        return round(time.perf_counter() - self.__ts)
+    def inativa_coordenador(self):
+        while(True):
+            time.sleep(60)
+            if len(self.processos) > 0:
+                coordenador = self.processos[0].coordenador
+                if coordenador is not None:
+                    id = coordenador.id
+                    processo = self.retorna_processo(coordenador.id)
+                    processo.set_ativo(False)
+                    self.remove_coordenador()
+                    self.processos.remove(processo)
+                    coordenador.stop()
+                    processo.stop()
+                    print(f'******** Coordenador inativado {id}')
 
+    def get_coordenador(self):
+        if(len(self.processos) > 0 ):
+            return self.processos[0].coordenador
+        return None
 
-def gera_processo():
-    while(True):
-        ran_id = randint(0, 2048)
-        processos[ran_id] = Processo(ran_id)
-        time.sleep(40)
+    def gera_novo_coordenador(self):
+        if len(self.processos):
+            print('\n')
+            print(f'******** Elegendo um novo coordenador randômico')
+            print(f'******** Processos ativos: {self.processos_ativos()}')
+            processo = self.processos[randint(0, len(self.processos) - 1)]
+            coordenador = Coordenador(processo.id)
+            print(f'{processo.tag} Novo coordenador!')
+            self.adicionar_coordenador_processos(coordenador)
 
+    def adicionar_coordenador_processos(self, coordenador):
+        print(f'******** Notificando processos do novo coordenador')
+        for p in self.processos:
+            p.set_coordenador(coordenador)
+        print('\n')
 
-#Execução principal
-processos = dict()
-threading.Thread(target=gera_processo).start()
-print('sim')
+    def processos_ativos(self):
+        s = []
+        for p in self.processos:
+            s.append(p.id)
+        return s
 
+    def retorna_processo(self, id):
+        for p in self.processos:
+            if p.id == id:
+                return p
+
+    def remove_coordenador(self):
+        for p in self.processos:
+            p.set_coordenador(None)
+
+    def verifica_id_existente(self, id):
+        for i in self.processos:
+            if i.id == id:
+                return False
+        return True
+
+    def processa_recurso(self, processo):
+        coordenador = self.processos[0].coordenador
+        if coordenador is not None and coordenador.isRecursoHabilitado == False:
+            print(f'******** Coordenador {coordenador.id} concede acesso ao processo {processo.id}')
+            print(f'{processo.tag} Iniciado processo do recurso!')
+            coordenador.isRecursoHabilitado = True
+            sleep = randint(20, 25)
+            time.sleep(sleep)
+            print(f'{processo.tag} Recurso processado em {sleep}s!')
+            print(
+                f'******** O processo {processo.id} informa ao coordenador que o recurso foi liberado')
+            print('\n')
+            coordenador.isRecursoHabilitado = False
+            self.remover_fila(coordenador, processo)
+
+    def run(self):
+        Thread(target=self.gera_processo).start()
+        Thread(target=self.inativa_coordenador).start()
+
+if __name__ == '__main__':
+    Processos().run()
